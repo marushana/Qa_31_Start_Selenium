b81ea5fa4e104e0f06ccdc6eeee5e8cd104afd7b
marushana <marushana@yandex.ru>
2025-11-04 20:18:07 +0200

    add login Tests

---
+++ src/test/java/StartSelenium.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class StartSelenium {
+    WebDriver wd;
+
+
+    @Test
+    public void test(){
+        wd = new ChromeDriver();
+        //wd.get("https://telranedu.web.app/");//without history
+        wd.navigate().to("https://telranedu.web.app/home");
+        wd.navigate().back();
+        wd.navigate().forward();
+        wd.navigate().refresh();
+
+
+        //wd.close();//one tab and close browser
+        wd.quit();//all tubs and close browser
+
+
+    }
+}
