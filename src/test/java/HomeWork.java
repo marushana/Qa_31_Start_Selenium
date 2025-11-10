cf122c513fedf2c999c00e9673eda54c27c9c4da
marushana <marushana@yandex.ru>
2025-11-10 13:13:49 +0200

    Create HomeWork.java

---
+++ src/test/java/HomeWork.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HomeWork {
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.get("https://telranedu.web.app/login");
+    }
+
+    @Test
+    public void locators(){
+        //by tagName
+
+        //by class
+
+        //by id
+
+        //by attribute
+
+        //one of elements find by attribute > start, end, contains
+
+
+
+    }
+}
