3daaedb440255461ad6ad7012154d75bf8e9432e
marushana <marushana@yandex.ru>
2025-11-10 16:10:33 +0200

    HomeWork done

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,14 +17,38 @@ public class HomeWork {
     @Test
     public void locators(){
         //by tagName
+        WebElement form = wd.findElement(By.tagName("form"));
+        WebElement formCss = wd.findElement(By.cssSelector("form"));
+        WebElement h1 = wd.findElement(By.tagName("h1"));
+        WebElement h1Css = wd.findElement(By.cssSelector("h1"));
+        WebElement br = wd.findElement(By.tagName("br"));
+        WebElement brCss = wd.findElement(By.cssSelector("br"));
+
 
         //by class
+        WebElement navbar = wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        WebElement navbarCss = wd.findElement(By.cssSelector(".navbar-component_nav__1X_4m"));
+        WebElement login = wd.findElement(By.className("login_login__3EHKB"));
+        WebElement loginCss = wd.findElement(By.cssSelector(".login_login__3EHKB"));
+
 
         //by id
+        WebElement root = wd.findElement(By.id("root"));
+        WebElement rootCss = wd.findElement(By.cssSelector("#root"));
 
         //by attribute
+        WebElement homeHref = wd.findElement(By.cssSelector("[href = '/home']"));
+        WebElement aboutHref = wd.findElement(By.cssSelector("[href = '/about']"));
+        WebElement loginHref = wd.findElement(By.cssSelector("[href = '/login']"));
+
 
         //one of elements find by attribute > start, end, contains
+        WebElement emailInput = wd.findElement(By.cssSelector("[placeholder ^= 'Em']"));
+        WebElement passwordInput = wd.findElement(By.cssSelector("[placeholder *= 'ass']"));
+        WebElement loginSubmit = wd.findElement(By.cssSelector("[name $= 'sword']"));
+        WebElement registrationSubmit = wd.findElement(By.cssSelector("br:last-child"));
+        
+
 
 
 
